00cbd699de27eb5ff1550a6fba1b912046431a71
WithertMaster <76103785+KotikmurmurRUS@users.noreply.github.com>
2023-11-16 16:59:41 +0100

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,11 @@
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+  loader.style.display = "none";
+})
+
+function openW(){
+window.open("https://chat.whatsapp.com/FAK6L5uYHVp1v9oCAahiOT")
+}
+function openD(){
+window.open("https://discord.com/invite/tUFUcpWeYK")
+}
