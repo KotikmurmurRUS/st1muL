9011aaaf37dc64670304bcc30e5be327144cc5c1
WithertMaster <76103785+KotikmurmurRUS@users.noreply.github.com>
2023-12-07 19:32:48 +0100

    Update programmation.js

--- programmation.js
+++ programmation.js
@@ -3,7 +3,6 @@ window.addEventListener("load", function() {
   loader.style.display = "none";
 })
 
-
 // Function to add or update a cookie
 function addCookie(name, value, days) {
   var expires = "";
@@ -13,15 +12,27 @@ function addCookie(name, value, days) {
     expires = "; expires=" + date.toUTCString();
   }
   document.cookie = name + "=" + value + expires + "; path=/";
+  console.log("Added new cookie named " + name + "!")
 }
 
 function checkUserCookie() {
   var userCookieValue = getCookie('User');
   if (userCookieValue === 'VLNfest2980_WEB') {
-    console.log('User cookie has a value of "Test".');
+    console.log('Programmation menu unlocked.');
+    document.getElementById("l1").style.background = "#444";
+    document.getElementById("l2").style.background = "#444";
+    document.getElementById("l3").style.background = "#444";
+    document.getElementById("l4").style.background = "#c44";
+    document.getElementById("l5").style.background = "#c44";
+    document.getElementById("l1t").innerHTML = "Radio 2";
+    document.getElementById("l1t").innerHTML = "DJ Nelasta";
+    document.getElementById("l1t").innerHTML = "Black Coffee";
+    document.getElementById("l1t").innerHTML = "Quiet";
+    document.getElementById("l1t").innerHTML = "Quiet";
     // Do something here if the condition is met
   } else {
-    console.log('User cookie does not have a value of "Test" or the cookie does not exist.');
+    console.log('Cookie does not exist.');
+    document.getElementById("infoDesk").style.display = "none";
   }
 }
 
@@ -31,6 +42,10 @@ if (userCookieValue === 'test') {
   console.log('User cookie has a value of "test".');
 } else {
   console.log('User cookie does not have a value of "test".');
+}
+
+function removeCookie(name) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 }
 
 // Function to get the value of a cookie by name
